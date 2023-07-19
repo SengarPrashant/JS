5f1ce42728833bd957eaa710dee679cbb64af3ed
prashant89m <prashant89m@gmail.com>
2023-07-19 15:58:41 +0530

    zip

--- webworker/worker.js
+++ webworker/worker.js
@@ -1,11 +1,14 @@
-self.addEventListener('message',(e)=>{
-    console.log(e.data)
-    if(e.data=='longRunnigTask'){
-        var count=0;
-        for (let index = 0; index < 3900000000; index++) {
-            count +=index;
-        }
-        self.postMessage({message:count})
+self.addEventListener('message', (e) => {
+    console.log(e.data);
+    switch (e.data) {
+        case "longRunnigTask":
+            var count = 0;
+            for (let index = 0; index < 3900000000; index++) {
+                count += index;
+            }
+            self.postMessage({ message: count });
+            break;
+        default :
+            break;
     }
-    
-})
+});
